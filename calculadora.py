16afe0a3c6e089569e65590f66b9f91fda7662ca
Yessenia Salinas <ye.salinas@duocuc.cl>
2026-06-17 21:05:41 -0400

    Agrega menu a la calculadora

--- calculadora.py
+++ calculadora.py
@@ -1,7 +1,15 @@
-print("Calculadora simple")
+print("Calculadora con menu")
 
 num1 = int(input("Ingrese numero 1: "))
 num2 = int(input("Ingrese numero 2: "))
 
-print("La suma es:", num1 + num2)
-print("La resta es:", num1 - num2)
+print("1.- Sumar")
+print("2.- Restar")
+opcion = input("Seleccione una opcion: ")
+
+if opcion == "1":
+    print("La suma es:", num1 + num2)
+elif opcion == "2":
+    print("La resta es:", num1 - num2)
+else:
+    print("Opcion no valida")
