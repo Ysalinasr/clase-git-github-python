e96f43ed968150960b2fe0ecbb5cf44408bfa08b
Yessenia Salinas <ye.salinas@duocuc.cl>
2026-06-17 20:59:29 -0400

    Agrega calculadora basica en Python

---
+++ calculadora.py
@@ -0,0 +1,7 @@
+print("Calculadora simple")
+
+num1 = int(input("Ingrese numero 1: "))
+num2 = int(input("Ingrese numero 2: "))
+
+print("La suma es:", num1 + num2)
+print("La resta es:", num1 - num2)
